ddbc4c638063f2efd815d4d2e7a5001aa6be9426
Phanyapat Chalermchai <kayzazz1@hotmail.com>
2021-04-28 15:24:15 +0700

    First Commit

---
+++ HomeWork/src/com/coding/homework/Homework.java
@@ -0,0 +1,23 @@
+package com.coding.homework;
+
+public class Homework {
+	public static void main(String [] args) {
+		//System.out.println("Hello World");
+		
+		/* This is my homework
+			CTF020018 kayzazz1@hotmail.com
+		*/
+		
+		int myInt = 999;
+		boolean myBoolean = true;
+		String myString = "CTF020018";
+		double myDouble = 6.5;
+		byte myByte = 20;
+		
+		System.out.println("This is a Int " + myInt);
+		System.out.println("This is a Boolean " + myBoolean);
+		System.out.println("This is a String " + myString);
+		System.out.println("This is a Double " + myDouble);
+		System.out.println("This is a Byte " + myByte);
+	}
+}
