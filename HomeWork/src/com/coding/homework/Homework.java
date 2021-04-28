a6d55bbbaa2cd07d7cde2024530c9828b5881fbc
Phanyapat Chalermchai <kayzazz1@hotmail.com>
2021-04-28 15:54:38 +0700

    finsh

--- HomeWork/src/com/coding/homework/Homework.java
+++ HomeWork/src/com/coding/homework/Homework.java
@@ -2,22 +2,29 @@ package com.coding.homework;
 
 public class Homework {
 	public static void main(String [] args) {
-		//System.out.println("Hello World");
+		bark();
 		
-		/* This is my homework
-			CTF020018 kayzazz1@hotmail.com
-		*/
+		float myFloat = 13.8f;
+		int myFloatToInt = (int)myFloat;
+		System.out.println("My Float to Int -> " + myFloatToInt);
 		
-		int myInt = 999;
-		boolean myBoolean = true;
-		String myString = "CTF020018";
-		double myDouble = 6.5;
-		byte myByte = 20;
+		int myInt = 50;
+		float myIntToFloat = (float)myInt;
+		System.out.println("My Int to Float -> " + myIntToFloat);
 		
-		System.out.println("This is a Int " + myInt);
-		System.out.println("This is a Boolean " + myBoolean);
-		System.out.println("This is a String " + myString);
-		System.out.println("This is a Double " + myDouble);
-		System.out.println("This is a Byte " + myByte);
+		double myDouble = 9.9;
+		float myDoubleToFloat = (float)myDouble;
+		System.out.println("My Double to Float -> " + myDoubleToFloat);
+		
+		char myChar = 'a';
+		int myCharToInt = (int)myChar;
+		System.out.println("My Char to Int -> " + myCharToInt);
+		
+		final String hello = "Hello";
+		hello = "World";
+	}
+	public static void bark() {
+		String dogName = "Kaka";
+		System.out.println("The Dog name = " + dogName + " bark");
 	}
 }
