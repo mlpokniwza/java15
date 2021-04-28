d016711d4f9247a26d11af6cdabcf330c0151c4a
Phanyapat Chalermchai <kayzazz1@hotmail.com>
2021-04-28 16:03:11 +0700

    finsh

--- HomeWork/src/com/coding/homework/Homework.java
+++ HomeWork/src/com/coding/homework/Homework.java
@@ -2,29 +2,18 @@ package com.coding.homework;
 
 public class Homework {
 	public static void main(String [] args) {
-		bark();
+		int i = 20;
+		for(int count=0; count<5; count++) {
+			i++;
+		}
+		System.out.println(i);
+		for(int count=0; count<5; count++) {
+			i--;
+		}
+		System.out.println(i);
 		
-		float myFloat = 13.8f;
-		int myFloatToInt = (int)myFloat;
-		System.out.println("My Float to Int -> " + myFloatToInt);
+		System.out.println( 5.5f < 7.8f && 3.2f > 2.1f );
 		
-		int myInt = 50;
-		float myIntToFloat = (float)myInt;
-		System.out.println("My Int to Float -> " + myIntToFloat);
-		
-		double myDouble = 9.9;
-		float myDoubleToFloat = (float)myDouble;
-		System.out.println("My Double to Float -> " + myDoubleToFloat);
-		
-		char myChar = 'a';
-		int myCharToInt = (int)myChar;
-		System.out.println("My Char to Int -> " + myCharToInt);
-		
-		final String hello = "Hello";
-		hello = "World";
-	}
-	public static void bark() {
-		String dogName = "Kaka";
-		System.out.println("The Dog name = " + dogName + " bark");
+		System.out.println( 'a' == 'b' || 'b' == 'c' );
 	}
 }
