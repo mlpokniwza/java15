377e4d51bf53f97d28a147fc809dc2238ef00fd9
Phanyapat Chalermchai <kayzazz1@hotmail.com>
2021-04-28 17:26:32 +0700

    5

--- HomeWork/src/com/coding/homework/Homework.java
+++ HomeWork/src/com/coding/homework/Homework.java
@@ -4,62 +4,16 @@ import java.util.Scanner;
 
 public class Homework {
 	public static void main(String[] args) {
-		// Lab 9
-		// 6.1
-		int i = 8;
-		while (i <= 10) {
-			System.out.print(i  + " ");
-			i++;
-		}
-		System.out.println();
-		// 6.2
-		int sum1 = 0;
-		for(int count=0; count<10; count++) {
-			sum1 += count;
-		}
-		System.out.println(sum1);
-		// 6.3
-		int sum2 = 0;
-		for(int count=1; count<=100; count++) {
-			if(count % 12 == 0) {
-				sum2 += count;
-			}
-		}
-		System.out.println(sum2);
-		// 6.4
-		int myArray[] = {1, 2, 3, 4, 5};
-		for(int counter : myArray) {
-			System.out.print(counter + " ");
-		}
-		System.out.println();
-		
-		// Lab 7
-		// 7.1
-		int myCount = 20;
-		do {
-			System.out.print(myCount + " ");
-			myCount--;
-		}while(myCount >= 1);
-		System.out.println();
-		// 7.2
+		String name = askName();
+		greet(name);
+	}
+	public static String askName() {
 		Scanner myScan = new Scanner(System.in);
-		System.out.print("Input number : ");
-		int myInput = myScan.nextInt();
-		do {
-			if(myInput % 2 == 1) {
-				break;
-			}
-			System.out.print("Input number : ");
-			myInput = myScan.nextInt();
-		}while(true);
-		
-		// Lab 8
-		for(int count=20; count>0; count--) {
-			if(count == 11) {
-				System.out.print(count);
-				break;
-			}
-			System.out.print("Count : " + count);
-		}
+		System.out.print("What is your name : ");
+		String userName = myScan.next();
+		return userName;
+	}
+	public static void greet(String name) {
+		System.out.println("Hello " + name);
 	}
 }
