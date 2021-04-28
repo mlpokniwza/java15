d2a7bfefecdd893df6d9b386cd14a7b7400cd73b
Phanyapat Chalermchai <kayzazz1@hotmail.com>
2021-04-28 16:12:40 +0700

    hw4.1

--- HomeWork/src/com/coding/homework/Homework.java
+++ HomeWork/src/com/coding/homework/Homework.java
@@ -2,18 +2,22 @@ package com.coding.homework;
 
 public class Homework {
 	public static void main(String [] args) {
-		int i = 20;
-		for(int count=0; count<5; count++) {
-			i++;
-		}
-		System.out.println(i);
-		for(int count=0; count<5; count++) {
-			i--;
-		}
-		System.out.println(i);
+		int score = 55;
+		boolean isHandSome = false;
 		
-		System.out.println( 5.5f < 7.8f && 3.2f > 2.1f );
 		
-		System.out.println( 'a' == 'b' || 'b' == 'c' );
+		if (isHandSome == true) {
+			System.out.println("Good");
+		}
+		else if (score >= 80) {
+			System.out.println("Good");
+		}
+		else if (score >= 50 && score <= 79) {
+			System.out.println("normal");
+		}
+		else {
+			System.out.println("fail");
+		}
+		
 	}
 }
