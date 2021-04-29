1c7683d199a0858ca0851166f1221c0057937956
Phanyapat Chalermchai <kayzazz1@hotmail.com>
2021-04-29 14:16:23 +0700

    10-3

---
+++ HomeWork/src/com/coding/oobLab/Employee.java
@@ -0,0 +1,30 @@
+package com.coding.oobLab;
+
+public class Employee {
+	public String firstname;
+	public String lastname;
+	public String position;
+	private int salary;
+
+	public Employee () {
+	this.firstname = "Anonymous";
+	this.lastname = "Anonymous";
+	this.position = "Anonymous";
+	this.salary = 9000; // default salary
+	}
+
+	public Employee (String firstnameInput, String lastnameInput, String positionInput, int salaryInput) {
+	this.firstname = firstnameInput;
+	this.lastname = lastnameInput;
+	this.position = positionInput;
+	this.salary = salaryInput;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+	
+	public void checkPosition() {
+		System.out.println(this.position);
+	}
+}
---
+++ HomeWork/src/com/coding/oobLab/Main.java
@@ -0,0 +1,9 @@
+package com.coding.oobLab;
+
+public class Main {
+	public static void main(String[] args) {
+		Employee[] Programmers = new Employee[2];
+		Programmers[0] = new Employee("Phanyapat", "Chalermchai", "Java", 50000);
+		Programmers[1] = new Employee("Puriwat", "Jitrakyol", "Python", 60000);
+	}
+}
